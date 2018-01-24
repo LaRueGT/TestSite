6ae2020c06e5ba992dfb656e3bb2aa21b9d05426
Garrett LaRue <gtlarue@gmail.com>
2018-01-24 13:02:05 -0500

    script defer

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,14 @@
-let navBar;
-let navTop;
-
-window.addEventListener("DOMContentLoaded", function(){
-    navBar = document.querySelector("nav");
-    navTop = navBar.offsetTop;
-
-    window.addEventListener("scroll", stickyNavBar);
-});
+const navBar = document.querySelector("nav");
+const navTop = navBar.offsetTop;
 
 function stickyNavBar(){
-    console.log("scrolled")
     if (window.scrollY >= navTop){
         navBar.classList.add("sticky");
     } else {
         navBar.classList.remove("sticky");
     }
 }
+
+window.addEventListener("scroll", stickyNavBar);
+
 
