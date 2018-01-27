2e3664b02583de6a1f6b553e34ca2345a016f010
Garrett LaRue <gtlarue@gmail.com>
2018-01-27 13:45:12 -0500

    flexbox stretching, alignment. link hover padding. nav box shadow

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,24 @@
 const navBar = document.querySelector("nav");
 const navTop = navBar.offsetTop;
 
-function stickyNavBar(){
-    if (window.scrollY >= navTop){
+const bookIndicator = document.querySelector(".book-indicator");
+const dropMenu = document.querySelector(".drop-menu");
+
+function toggleDropDown() {
+    if (dropMenu.style.display === "none") {
+        dropMenu.style.display = "flex";
+    } else {
+        dropMenu.style.display = "none";
+    }
+}
+
+function stickyNavBar() {
+    if (window.scrollY >= navTop) {
         navBar.classList.add("sticky");
     } else {
         navBar.classList.remove("sticky");
     }
 }
 
+bookIndicator.addEventListener("click", toggleDropDown);
 window.addEventListener("scroll", stickyNavBar);
